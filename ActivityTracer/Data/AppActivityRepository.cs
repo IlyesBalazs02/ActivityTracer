a9bf35fddd588779ad7810c626f5aad5acd3fd10
IlyesBalazs <ilyesbalazs02@gmail.com>
2024-06-14 09:00:01 +0200

    AppActivity repository

--- ActivityTracer/Data/AppActivityRepository.cs
+++ ActivityTracer/Data/AppActivityRepository.cs
@@ -1,7 +1,60 @@
-﻿namespace ActivityTracer.Data
-{
-	public class AppActivityRepository
-	{
+﻿using ActivityTracer.Models;
+using Microsoft.AspNetCore.Components.Web.Virtualization;
 
+namespace ActivityTracer.Data
+{
+	public class AppActivityRepository : IAppActivityRepository
+	{
+		ActivityDbContext context;
+
+		public AppActivityRepository(ActivityDbContext context)
+		{
+			this.context = context;
+		}
+
+		public void Create(AppActivity activity)
+		{
+			context.Activities.Add(activity);
+			context.SaveChanges();
+		}
+
+		public IEnumerable<AppActivity> Read()
+		{
+			return context.Activities;
+		}
+
+
+		public AppActivity? ReadFromId(string id)
+		{
+			return context.Activities.FirstOrDefault(t => t.Id == id);
+		}
+
+		public void Update(AppActivity activity)
+		{
+			var old = ReadFromId((string)activity.Id);
+
+			if (old != null)
+			{
+				throw new Exception("Object not found");
+			}
+
+			foreach (var prop in activity.GetType().GetProperties())
+			{
+				var value = prop.GetValue(activity);
+
+				var oldProp = old.GetType().GetProperty(prop.Name);
+
+				oldProp.SetValue(old, value);
+			}
+
+			context.SaveChanges();
+		}
+
+		public void DeleteFromId(string id)
+		{
+			var activity = ReadFromId(id);
+			context.Activities.Remove(activity);
+			context.SaveChanges();
+		}
 	}
 }
---
+++ ActivityTracer/Data/IAppActivityRepository.cs
@@ -0,0 +1,13 @@
+﻿using ActivityTracer.Models;
+
+namespace ActivityTracer.Data
+{
+	public interface IAppActivityRepository
+	{
+		void Create(AppActivity activity);
+		void DeleteFromId(string id);
+		IEnumerable<AppActivity> Read();
+		AppActivity? ReadFromId(string id);
+		void Update(AppActivity activity);
+	}
+}
