b974d95d09021c7cea944fb04637ffbc5a226e9d
IlyesBalazs <ilyesbalazs02@gmail.com>
2024-06-16 17:12:44 +0200

    Migration added

--- ActivityTracer/Data/ActivityDbContext.cs
+++ ActivityTracer/Data/ActivityDbContext.cs
@@ -1,11 +1,13 @@
 ﻿using ActivityTracer.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ActivityTracer.Data
 {
-	public class ActivityDbContext : DbContext
+	public class ActivityDbContext : IdentityDbContext
 	{
 		public DbSet<AppActivity> Activities { get; set; }
+		public DbSet<SiteUser> Users { get; set; }
 
         public ActivityDbContext(DbContextOptions<ActivityDbContext> opt) : base(opt) { }
 
---
+++ ActivityTracer/Models/SiteUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ActivityTracer.Models
+{
+	public class SiteUser : IdentityUser
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+
+		public string ContentType { get; set; }
+		public byte[] Data { get; set; }
+	}
+}
