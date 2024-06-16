d2531bf5849c243b53f7912c2a298d07a71ee9e5
IlyesBalazs <ilyesbalazs02@gmail.com>
2024-06-16 23:11:54 +0200

    saving profile picture from facebook login

--- ActivityTracer/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
+++ ActivityTracer/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
@@ -18,217 +18,235 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
+using System.Net;
+using Newtonsoft.Json;
 
 namespace ActivityTracer.Areas.Identity.Pages.Account
 {
-    [AllowAnonymous]
-    public class ExternalLoginModel : PageModel
-    {
-        private readonly SignInManager<SiteUser> _signInManager;
-        private readonly UserManager<SiteUser> _userManager;
-        private readonly IUserStore<SiteUser> _userStore;
-        private readonly IUserEmailStore<SiteUser> _emailStore;
-        private readonly IEmailSender _emailSender;
-        private readonly ILogger<ExternalLoginModel> _logger;
+	[AllowAnonymous]
+	public class ExternalLoginModel : PageModel
+	{
+		private readonly SignInManager<SiteUser> _signInManager;
+		private readonly UserManager<SiteUser> _userManager;
+		private readonly IUserStore<SiteUser> _userStore;
+		private readonly IUserEmailStore<SiteUser> _emailStore;
+		private readonly IEmailSender _emailSender;
+		private readonly ILogger<ExternalLoginModel> _logger;
 
-        public ExternalLoginModel(
-            SignInManager<SiteUser> signInManager,
-            UserManager<SiteUser> userManager,
-            IUserStore<SiteUser> userStore,
-            ILogger<ExternalLoginModel> logger,
-            IEmailSender emailSender)
-        {
-            _signInManager = signInManager;
-            _userManager = userManager;
-            _userStore = userStore;
-            _emailStore = GetEmailStore();
-            _logger = logger;
-            _emailSender = emailSender;
-        }
+		public ExternalLoginModel(
+			SignInManager<SiteUser> signInManager,
+			UserManager<SiteUser> userManager,
+			IUserStore<SiteUser> userStore,
+			ILogger<ExternalLoginModel> logger,
+			IEmailSender emailSender)
+		{
+			_signInManager = signInManager;
+			_userManager = userManager;
+			_userStore = userStore;
+			_emailStore = GetEmailStore();
+			_logger = logger;
+			_emailSender = emailSender;
+		}
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        [BindProperty]
-        public InputModel Input { get; set; }
+		/// <summary>
+		///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+		///     directly from your code. This API may change or be removed in future releases.
+		/// </summary>
+		[BindProperty]
+		public InputModel Input { get; set; }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        public string ProviderDisplayName { get; set; }
+		/// <summary>
+		///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+		///     directly from your code. This API may change or be removed in future releases.
+		/// </summary>
+		public string ProviderDisplayName { get; set; }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        public string ReturnUrl { get; set; }
+		/// <summary>
+		///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+		///     directly from your code. This API may change or be removed in future releases.
+		/// </summary>
+		public string ReturnUrl { get; set; }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        [TempData]
-        public string ErrorMessage { get; set; }
+		/// <summary>
+		///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+		///     directly from your code. This API may change or be removed in future releases.
+		/// </summary>
+		[TempData]
+		public string ErrorMessage { get; set; }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        public class InputModel
-        {
+		/// <summary>
+		///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+		///     directly from your code. This API may change or be removed in future releases.
+		/// </summary>
+		/// 
+		public class TokenModel
+		{
+			public string access_token { get; set; }
+			public string token_type { get; set; }
+		}
 
-            [Required]
-            [EmailAddress]
-            public string Email { get; set; }
-            
-            [Required]
-            [StringLength(100)]
-            public string FirstName { get; set; }
-            
-            [Required]
-            [StringLength(100)]
-            public string LastName { get; set; }
+		public class InputModel
+		{
 
-        }
-        
-        public IActionResult OnGet() => RedirectToPage("./Login");
+			[Required]
+			[EmailAddress]
+			public string Email { get; set; }
 
-        public IActionResult OnPost(string provider, string returnUrl = null)
-        {
-            // Request a redirect to the external login provider.
-            var redirectUrl = Url.Page("./ExternalLogin", pageHandler: "Callback", values: new { returnUrl });
-            var properties = _signInManager.ConfigureExternalAuthenticationProperties(provider, redirectUrl);
-            return new ChallengeResult(provider, properties);
-        }
+			[Required]
+			[StringLength(100)]
+			public string FirstName { get; set; }
 
-        public async Task<IActionResult> OnGetCallbackAsync(string returnUrl = null, string remoteError = null)
-        {
-            returnUrl = returnUrl ?? Url.Content("~/");
-            if (remoteError != null)
-            {
-                ErrorMessage = $"Error from external provider: {remoteError}";
-                return RedirectToPage("./Login", new { ReturnUrl = returnUrl });
-            }
-            var info = await _signInManager.GetExternalLoginInfoAsync();
-            if (info == null)
-            {
-                ErrorMessage = "Error loading external login information.";
-                return RedirectToPage("./Login", new { ReturnUrl = returnUrl });
-            }
+			[Required]
+			[StringLength(100)]
+			public string LastName { get; set; }
 
-            // Sign in the user with this external login provider if the user already has a login.
-            var result = await _signInManager.ExternalLoginSignInAsync(info.LoginProvider, info.ProviderKey, isPersistent: false, bypassTwoFactor: true);
-            if (result.Succeeded)
-            {
-                _logger.LogInformation("{Name} logged in with {LoginProvider} provider.", info.Principal.Identity.Name, info.LoginProvider);
-                return LocalRedirect(returnUrl);
-            }
-            if (result.IsLockedOut)
-            {
-                return RedirectToPage("./Lockout");
-            }
-            else
-            {
-                // If the user does not have an account, then ask the user to create an account.
-                ReturnUrl = returnUrl;
-                ProviderDisplayName = info.ProviderDisplayName;
-                if (info.Principal.HasClaim(c => c.Type == ClaimTypes.Email))
-                {
-                    Input = new InputModel
-                    {
-                        Email = info.Principal.FindFirstValue(ClaimTypes.Email),
-                        FirstName = info.Principal.FindFirstValue(ClaimTypes.Surname),
-                        LastName = info.Principal.FindFirstValue(ClaimTypes.GivenName)
-                    };
-                }
-                return Page();
-            }
-        }
+			public string PictureUrl { get; set; }
+		}
 
-        public async Task<IActionResult> OnPostConfirmationAsync(string returnUrl = null)
-        {
-            returnUrl = returnUrl ?? Url.Content("~/");
-            // Get the information about the user from the external login provider
-            var info = await _signInManager.GetExternalLoginInfoAsync();
-            if (info == null)
-            {
-                ErrorMessage = "Error loading external login information during confirmation.";
-                return RedirectToPage("./Login", new { ReturnUrl = returnUrl });
-            }
+		public IActionResult OnGet() => RedirectToPage("./Login");
 
-            if (ModelState.IsValid)
-            {
-                var user = CreateUser();
-                user.FirstName = Input.FirstName;
-                user.LastName = Input.LastName;
+		public IActionResult OnPost(string provider, string returnUrl = null)
+		{
+			// Request a redirect to the external login provider.
+			var redirectUrl = Url.Page("./ExternalLogin", pageHandler: "Callback", values: new { returnUrl });
+			var properties = _signInManager.ConfigureExternalAuthenticationProperties(provider, redirectUrl);
+			return new ChallengeResult(provider, properties);
+		}
 
-                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
-                await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+		public async Task<IActionResult> OnGetCallbackAsync(string returnUrl = null, string remoteError = null)
+		{
+			returnUrl = returnUrl ?? Url.Content("~/");
+			if (remoteError != null)
+			{
+				ErrorMessage = $"Error from external provider: {remoteError}";
+				return RedirectToPage("./Login", new { ReturnUrl = returnUrl });
+			}
+			var info = await _signInManager.GetExternalLoginInfoAsync();
+			if (info == null)
+			{
+				ErrorMessage = "Error loading external login information.";
+				return RedirectToPage("./Login", new { ReturnUrl = returnUrl });
+			}
 
-                var result = await _userManager.CreateAsync(user);
-                if (result.Succeeded)
-                {
-                    result = await _userManager.AddLoginAsync(user, info);
-                    if (result.Succeeded)
-                    {
-                        _logger.LogInformation("User created an account using {Name} provider.", info.LoginProvider);
+			// Sign in the user with this external login provider if the user already has a login.
+			var result = await _signInManager.ExternalLoginSignInAsync(info.LoginProvider, info.ProviderKey, isPersistent: false, bypassTwoFactor: true);
+			if (result.Succeeded)
+			{
+				_logger.LogInformation("{Name} logged in with {LoginProvider} provider.", info.Principal.Identity.Name, info.LoginProvider);
+				return LocalRedirect(returnUrl);
+			}
+			if (result.IsLockedOut)
+			{
+				return RedirectToPage("./Lockout");
+			}
+			else
+			{
+				// If the user does not have an account, then ask the user to create an account.
+				ReturnUrl = returnUrl;
+				ProviderDisplayName = info.ProviderDisplayName;
+				if (info.Principal.HasClaim(c => c.Type == ClaimTypes.Email))
+				{
+					Input = new InputModel
+					{
+						Email = info.Principal.FindFirstValue(ClaimTypes.Email),
+						FirstName = info.Principal.FindFirstValue(ClaimTypes.Surname),
+						LastName = info.Principal.FindFirstValue(ClaimTypes.GivenName),
+					};
+					var access_token_json = new WebClient().DownloadString("https://graph.facebook.com/oauth/access_token?client_id=950452413482166&client_secret=427384153dc998e129210735438c2faa&grant_type=client_credentials");
+					var token = JsonConvert.DeserializeObject<TokenModel>(access_token_json);
+					Input.PictureUrl = $"https://graph.facebook.com/{info.Principal.FindFirstValue(ClaimTypes.NameIdentifier)}/picture?type=large&access_token={token.access_token}";
+				}
+				return Page();
+			}
+		}
 
-                        var userId = await _userManager.GetUserIdAsync(user);
-                        var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                        code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                        var callbackUrl = Url.Page(
-                            "/Account/ConfirmEmail",
-                            pageHandler: null,
-                            values: new { area = "Identity", userId = userId, code = code },
-                            protocol: Request.Scheme);
+		public async Task<IActionResult> OnPostConfirmationAsync(string returnUrl = null)
+		{
+			returnUrl = returnUrl ?? Url.Content("~/");
+			// Get the information about the user from the external login provider
+			var info = await _signInManager.GetExternalLoginInfoAsync();
+			if (info == null)
+			{
+				ErrorMessage = "Error loading external login information during confirmation.";
+				return RedirectToPage("./Login", new { ReturnUrl = returnUrl });
+			}
 
-                        await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                            $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+			if (ModelState.IsValid)
+			{
+				var user = CreateUser();
+				user.FirstName = Input.FirstName;
+				user.LastName = Input.LastName;
 
-                        // If account confirmation is required, we need to show the link if we don't have a real email sender
-                        if (_userManager.Options.SignIn.RequireConfirmedAccount)
-                        {
-                            return RedirectToPage("./RegisterConfirmation", new { Email = Input.Email });
-                        }
+				//todo kep feldolgozasa
+				var wc = new WebClient();
+				user.Data = wc.DownloadData(Input.PictureUrl);
+				user.ContentType = wc.ResponseHeaders["Content-type"];
 
-                        await _signInManager.SignInAsync(user, isPersistent: false, info.LoginProvider);
-                        return LocalRedirect(returnUrl);
-                    }
-                }
-                foreach (var error in result.Errors)
-                {
-                    ModelState.AddModelError(string.Empty, error.Description);
-                }
-            }
+				await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+				await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
 
-            ProviderDisplayName = info.ProviderDisplayName;
-            ReturnUrl = returnUrl;
-            return Page();
-        }
+				var result = await _userManager.CreateAsync(user);
+				if (result.Succeeded)
+				{
+					result = await _userManager.AddLoginAsync(user, info);
+					if (result.Succeeded)
+					{
+						_logger.LogInformation("User created an account using {Name} provider.", info.LoginProvider);
 
-        private SiteUser CreateUser()
-        {
-            try
-            {
-                return Activator.CreateInstance<SiteUser>();
-            }
-            catch
-            {
-                throw new InvalidOperationException($"Can't create an instance of '{nameof(SiteUser)}'. " +
-                    $"Ensure that '{nameof(SiteUser)}' is not an abstract class and has a parameterless constructor, or alternatively " +
-                    $"override the external login page in /Areas/Identity/Pages/Account/ExternalLogin.cshtml");
-            }
-        }
+						var userId = await _userManager.GetUserIdAsync(user);
+						var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+						code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+						var callbackUrl = Url.Page(
+							"/Account/ConfirmEmail",
+							pageHandler: null,
+							values: new { area = "Identity", userId = userId, code = code },
+							protocol: Request.Scheme);
 
-        private IUserEmailStore<SiteUser> GetEmailStore()
-        {
-            if (!_userManager.SupportsUserEmail)
-            {
-                throw new NotSupportedException("The default UI requires a user store with email support.");
-            }
-            return (IUserEmailStore<SiteUser>)_userStore;
-        }
-    }
+						await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+							$"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+						// If account confirmation is required, we need to show the link if we don't have a real email sender
+						if (_userManager.Options.SignIn.RequireConfirmedAccount)
+						{
+							return RedirectToPage("./RegisterConfirmation", new { Email = Input.Email });
+						}
+
+						await _signInManager.SignInAsync(user, isPersistent: false, info.LoginProvider);
+						return LocalRedirect(returnUrl);
+					}
+				}
+				foreach (var error in result.Errors)
+				{
+					ModelState.AddModelError(string.Empty, error.Description);
+				}
+			}
+
+			ProviderDisplayName = info.ProviderDisplayName;
+			ReturnUrl = returnUrl;
+			return Page();
+		}
+
+		private SiteUser CreateUser()
+		{
+			try
+			{
+				return Activator.CreateInstance<SiteUser>();
+			}
+			catch
+			{
+				throw new InvalidOperationException($"Can't create an instance of '{nameof(SiteUser)}'. " +
+					$"Ensure that '{nameof(SiteUser)}' is not an abstract class and has a parameterless constructor, or alternatively " +
+					$"override the external login page in /Areas/Identity/Pages/Account/ExternalLogin.cshtml");
+			}
+		}
+
+		private IUserEmailStore<SiteUser> GetEmailStore()
+		{
+			if (!_userManager.SupportsUserEmail)
+			{
+				throw new NotSupportedException("The default UI requires a user store with email support.");
+			}
+			return (IUserEmailStore<SiteUser>)_userStore;
+		}
+	}
 }
