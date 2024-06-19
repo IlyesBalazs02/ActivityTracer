650d260ccda4690cb94dcb3a66f0e7c0f7ba7272
IlyesBalazs <ilyesbalazs02@gmail.com>
2024-06-19 13:22:06 +0200

    ActivityClientWpf project created

---
+++ ActivityClientWpf/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ActivityClientWpf
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+	}
+
+}
